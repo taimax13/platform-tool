271afc711d64a5a4eddbeaac9ee0559f150f0d51
talexm <talexm@infibond.com>
2024-09-02 09:25:06 +0300

    localstack issue

--- eks.tf
+++ eks.tf
@@ -1,27 +1,64 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  cluster_name    = "${var.project_name}-${var.environment}-cluster"
-  cluster_version = "1.27"
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-  eks_managed_node_group_defaults = {
-    #ami_type       = "AL2_x86_64"
-    instance_types = var.instance_types
+  version = "~> 20.0"
+
+  cluster_name    = "my-cluster"
+  cluster_version = "1.30"
+
+  cluster_endpoint_public_access  = true
+
+  cluster_addons = {
+    coredns                = {}
+    eks-pod-identity-agent = {}
+    kube-proxy             = {}
+    vpc-cni                = {}
   }
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.public_subnets
+  control_plane_subnet_ids = module.vpc.private_subnets
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
+  }
+  create_cloudwatch_log_group = false
   eks_managed_node_groups = {
-    initial = {
-      min_size     = 1
-      max_size     = 3
+    example = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["m5.xlarge"]
+
+      min_size     = 2
+      max_size     = 10
       desired_size = 2
-      instance_types = var.instance_types
-      capacity_type  = "ON_DEMAND"
     }
   }
 
-  enable_irsa = true
+  # Cluster access entry
+  # To add the current caller identity as an administrator
+  enable_cluster_creator_admin_permissions = true
+
+  access_entries = {
+    # One access entry with a policy associated
+    example = {
+      kubernetes_groups = []
+      principal_arn     = "arn:aws:iam::123456789012:role/something"
+
+      policy_associations = {
+        example = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
+          access_scope = {
+            namespaces = ["default"]
+            type       = "namespace"
+          }
+        }
+      }
+    }
+  }
 
   tags = {
-    Environment = var.environment
-    Product     = var.project_name
+    Environment = "dev"
+    Terraform   = "true"
   }
 }
--- providers.tf
+++ providers.tf
@@ -1,9 +1,8 @@
-###https://docs.localstack.cloud/user-guide/integrations/terraform/
 provider "aws" {
   access_key                  = "test"
   secret_key                  = "test"
   region                      = "us-east-1"
-  s3_use_path_style           = false
+  s3_use_path_style           = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
@@ -20,17 +19,19 @@ provider "aws" {
     firehose       = "http://localhost:4566"
     iam            = "http://localhost:4566"
     kinesis        = "http://localhost:4566"
+    eks            = "http://localhost:4566"
     lambda         = "http://localhost:4566"
     rds            = "http://localhost:4566"
     redshift       = "http://localhost:4566"
     route53        = "http://localhost:4566"
-    s3             = "http://s3.localhost.localstack.cloud:4566"
+    s3             = "http://localhost:4566"
     secretsmanager = "http://localhost:4566"
     ses            = "http://localhost:4566"
     sns            = "http://localhost:4566"
     sqs            = "http://localhost:4566"
     ssm            = "http://localhost:4566"
     sts            = "http://localhost:4566"
+    cognitoidp    = "http://localhost:4566"
+    kms           = "http://localhost:4566"
   }
 }
-
